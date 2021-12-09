4992a346d81be42f7bc216e9ad7daaadadbf7a86
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2021-12-09 11:46:01 -0500

    inc.

--- src/components/SiteAlerts.tsx
+++ src/components/SiteAlerts.tsx
@@ -2,7 +2,7 @@ export default function SiteAlerts() {
   return (
     <div className="app">
       <div id="alerts">
-        <h3> → Learn what’s new with the ConnectPlus V2.</h3>
+        <h3> → Learn what’s new with the ConnectPlus V1.</h3>
       </div>
     </div>
   );
